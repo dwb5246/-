c162e813b3297cecb46b022051914d9ce6db9d91
dwb5246 <37902509+dwb5246@users.noreply.github.com>
2020-05-28 14:52:44 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ImageTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Text_FilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.text_box1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.text_box2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.text_box1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.text_box2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "签名抠图";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(267, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "印章抠图";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Text_FilePath
+            // 
+            this.Text_FilePath.AllowDrop = true;
+            this.Text_FilePath.Location = new System.Drawing.Point(90, 28);
+            this.Text_FilePath.Name = "Text_FilePath";
+            this.Text_FilePath.ReadOnly = true;
+            this.Text_FilePath.Size = new System.Drawing.Size(237, 21);
+            this.Text_FilePath.TabIndex = 4;
+            this.Text_FilePath.TabStop = false;
+            this.Text_FilePath.Text = "可拖入图片";
+            this.Text_FilePath.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.Text_FilePath.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "图片路径：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "相似度：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "差异度：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.text_box1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(21, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "签名抠图";
+            // 
+            // text_box1
+            // 
+            this.text_box1.DecimalPlaces = 2;
+            this.text_box1.Location = new System.Drawing.Point(63, 31);
+            this.text_box1.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.text_box1.Name = "text_box1";
+            this.text_box1.Size = new System.Drawing.Size(198, 21);
+            this.text_box1.TabIndex = 8;
+            this.text_box1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "说明：数值越小越清晰";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.text_box2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(21, 162);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(378, 84);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "印章抠图";
+            // 
+            // text_box2
+            // 
+            this.text_box2.Location = new System.Drawing.Point(63, 20);
+            this.text_box2.Name = "text_box2";
+            this.text_box2.Size = new System.Drawing.Size(198, 21);
+            this.text_box2.TabIndex = 9;
+            this.text_box2.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "说明：数值越大越清晰";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(333, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "选择图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 258);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Text_FilePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "大港油田-背景色透明化工具";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.text_box1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.text_box2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Text_FilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown text_box1;
+        private System.Windows.Forms.NumericUpDown text_box2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<byte> RList = new List<byte>();
+        List<byte> GList = new List<byte>();
+        List<byte> BList = new List<byte>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileInfo fileInfo = new FileInfo(Text_FilePath.Text);
+            if (!Directory.Exists(fileInfo.DirectoryName + @"\Image"))
+            {
+                Directory.CreateDirectory(fileInfo.DirectoryName + @"\Image");
+            }
+            using (FileStream stream = File.OpenRead(Text_FilePath.Text))
+            using (FileStream output = File.OpenWrite(string.Format(fileInfo.DirectoryName+@"\Image\new_"+ fileInfo.Name)))
+            using (Image<Rgba32> image = Image.Load(stream))
+            {
+                ClipMethod2(output, image);
+            }
+            MessageBox.Show(string.Format("文件已保存到->{0}", string.Format(fileInfo.DirectoryName + @"\Image\new_" + fileInfo.Name)), "提示");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileInfo fileInfo = new FileInfo(Text_FilePath.Text);
+            if (!Directory.Exists(fileInfo.DirectoryName + @"\Image"))
+            {
+                Directory.CreateDirectory(fileInfo.DirectoryName + @"\Image");
+            }
+            using (FileStream stream = File.OpenRead(Text_FilePath.Text))
+            using (FileStream output = File.OpenWrite(string.Format(fileInfo.DirectoryName + @"\Image\new_" + fileInfo.Name)))
+            using (Image<Rgba32> image = Image.Load(stream))
+            {
+                ClipMethod3(output, image);
+            }
+            MessageBox.Show(string.Format("文件已保存到->{0}", string.Format(fileInfo.DirectoryName + @"\Image\new_" + fileInfo.Name)), "提示");
+        }
+
+        private void ClipMethod(FileStream output, Image<Rgba32> image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    RList.Add(image[i, j].R);
+                    GList.Add(image[i, j].G);
+                    BList.Add(image[i, j].B);
+                }
+            }
+            byte mR = RList.Max();
+            byte mG = GList.Max();
+            byte mB = BList.Max();
+            Rgb24 rgb = new Rgb24(mR, mG, mB);
+            double max = 0;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    double curDis = HSV.distanceOf(rgb, image[i, j].Rgb);
+                    max = curDis > max ? curDis : max;
+                }
+            }
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    double curDis = HSV.distanceOf(rgb, image[i, j].Rgb) / max;
+                    Rgba32 item = image[i, j];
+                    if (curDis <= Convert.ToDouble(text_box1.Value))
+                    {
+                        item.A = 1;
+                        image[i, j] = item;
+                    }
+                }
+            }
+            image.SaveAsPng(output);
+        }
+
+        /// <summary>
+        /// 通过RGB每个差值移除背景
+        /// </summary>
+        /// <param name="image"></param>
+        private void ClipMethod1(FileStream output, Image<Rgba32> image)
+        {
+            double des = Convert.ToDouble(text_box1.Value);
+            Image<Rgba32> image2 = image.Clone();
+            image2.Mutate(x => x.Grayscale());
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Rgba32 item = image[i, j];
+                    if (Math.Abs(item.R - item.G) + Math.Abs(item.G - item.B) + Math.Abs(item.R - item.B) < des)
+                    {
+                        item.A = 1;
+                        image[i, j] = item;
+                    }
+                }
+            }
+            image.SaveAsPng(output);
+        }
+        /// <summary>
+        /// 通过RGB距离移除背景
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="image"></param>
+        private void ClipMethod2(FileStream output, Image<Rgba32> image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    RList.Add(image[i, j].R);
+                    GList.Add(image[i, j].G);
+                    BList.Add(image[i, j].B);
+                }
+            }
+            byte mR = RList.Max();
+            byte mG = GList.Max();
+            byte mB = BList.Max();
+            Rgb24 bgr = new Rgb24(mR, mG, mB);
+            double max = 0;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    double curDis = HSV.distanceOf(bgr, image[i, j].Rgb);
+                    max = curDis > max ? curDis : max;
+                }
+            }
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    double curDis = 1-HSV.distanceOf(bgr, image[i, j].Rgb) / max;
+                    Rgba32 item = image[i, j];
+                    if (curDis >= Convert.ToDouble(text_box1.Text))
+                    {
+                        item.A = 1;
+                        image[i, j] = item;
+                    }
+                }
+            }
+            image.SaveAsPng(output);
+        }
+
+        /// <summary>
+        /// 通过灰度图像，移除差距较小的像素
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="image"></param>
+        private void ClipMethod3(FileStream output, Image<Rgba32> image)
+        {
+            double des = Convert.ToDouble(text_box2.Text);
+            Image<Rgba32> image2 = image.Clone();
+            image2.Mutate(x => x.Grayscale());
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Rgba32 item = image[i, j];
+                    int distance = (int)DistanceOf(image2[i, j].Rgb, image[i, j].Rgb);
+                    if (distance <= des)
+                    {
+                        item.A = 1;
+                        image[i, j] = item;
+                    }
+                }
+            }
+            image.SaveAsPng(output);
+        }
+        public double DistanceOf(Rgb24 hsv1, Rgb24 hsv2)
+        {
+            double x = hsv1.R - hsv2.R;
+            double y = hsv1.G - hsv2.G;
+            double z = hsv1.B - hsv2.B;
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            Text_FilePath.Text = path;
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+                this.Text_FilePath.Cursor = System.Windows.Forms.Cursors.Arrow;  //指定鼠标形状（更好看）
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "选择图片文件";
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Text_FilePath.Text = openFileDialog.FileName;
+            }
+        }
+    }
+    public static class HSV
+    {
+        public static double distanceOf(Rgb24 hsv1, Rgb24 hsv2)
+        {
+            double x = hsv1.R - hsv2.R;
+            double y = hsv1.G - hsv2.G;
+            double z = hsv1.B - hsv2.B;
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+    }
+}
